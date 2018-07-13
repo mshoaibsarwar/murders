1b3afb7f233e847bb98aeb0bb34b674e9409e3b5
shoaib666 <shoa-ib@hotmail.com>
2018-07-14 04:47:54 +0500

    adding files

---
+++ analysis.R
@@ -0,0 +1,9 @@
+library(dslabs)
+library(dplyr)
+library(ggplot2)
+
+murders %>% mutate(abb=reorder(abb,rate))%>%
+  ggplot(aes(abb,rate))+
+  geom_bar(width = 0.5,stat = "identity",color="black")+
+  coord_flip()
+ggsave("figs/barplot.png")
